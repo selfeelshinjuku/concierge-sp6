6e743e9fde00e454b2e8f907a022a00a7f2aa6c8
selfeelshinjuku <selfeel@yacht.ocn.ne.jp>
2026-04-21 16:20:48 +0900

    Update config.js

--- index.html/assets/js/config.js
+++ index.html/assets/js/config.js
@@ -1,3 +1,5 @@
 export const APP_CONFIG = {
-  reservationFormUrl: 'https://script.google.com/macros/s/（ここに完動版GASのID）/exec',
+  reservationFormUrl: 'https://script.google.com/macros/s/
+https://script.google.com/macros/s/AKfycbx1xQzdKXhJaYZnwtHVvu_hS64Lr--hYpfWO3PH6dn_kFf2OVOlZPNG6bYTJa_z5Nrr/exec
+/exec',
 };
