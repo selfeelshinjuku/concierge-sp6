150b73540ad4b8468819c58f3fe0fd08d54073e3
selfeelshinjuku <selfeel@yacht.ocn.ne.jp>
2026-04-21 16:54:11 +0900

    Update config.js

--- index.html/assets/js/config.js
+++ index.html/assets/js/config.js
@@ -1,3 +1,3 @@
 export const APP_CONFIG = {
-  reservationFormUrl: 'https://script.google.com/macros/s/AKfycbx1xQzdKXhJaYZnwtHVvu_hS64Lr--hYpfWO3PH6dn_kFf2OVOlZPNG6bYTJa_z5Nrr/exec?selected=',
+  reservationFormUrl: 'https://ws.formzu.net/dist/S967546633/',
 };
